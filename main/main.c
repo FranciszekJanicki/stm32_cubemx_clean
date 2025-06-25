6a01e74af96268526af5a2fac518bbf899477370
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-06-26 01:12:03 +0200

    changes

--- main/main.c
+++ main/main.c
@@ -1,59 +1,7 @@
 #include "main.h"
-#include "gpio.h"
-#include "joint_manager.h"
-#include "joint_task.h"
-#include "stm32l4xx_hal.h"
-#include "tim.h"
-#include "usart.h"
-#include <stdio.h>
 
 int main(void)
 {
     HAL_Init();
     SystemClock_Config();
-
-    MX_GPIO_Init();
-    MX_TIM1_Init();
-    MX_TIM2_Init();
-    MX_USART2_UART_Init();
-
-    joint_manager_t manager = {.dir_port = GPIOA,
-                               .dir_pin = 1U << 10U,
-                               .delta_time = 0.01F,
-                               .pwm_channel = TIM_CHANNEL_4,
-                               .delta_timer = &htim2,
-                               .pwm_timer = &htim1};
-
-    joint_queue_initialize();
-    joint_task_initialize(&manager);
-
-    vTaskStartScheduler();
-}
-
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef* htim)
-{
-    BaseType_t task_woken = pdFALSE;
-
-    if (htim->Instance == TIM2) {
-        xTaskNotifyFromISR(joint_task_get_handle(),
-                           JOINT_NOTIFY_DELTA_TIMER,
-                           eSetBits,
-                           &task_woken);
-    }
-    if (htim->Instance == TIM3) {
-        HAL_IncTick();
-    }
-
-    portYIELD_FROM_ISR(task_woken);
-}
-
-void HAL_TIM_PWM_PulseFinishedCallback(TIM_HandleTypeDef* htim)
-{
-    BaseType_t task_woken = pdFALSE;
-
-    if (htim->Instance == TIM1) {
-        xTaskNotifyFromISR(joint_task_get_handle(), JOINT_NOTIFY_PWM_PULSE, eSetBits, &task_woken);
-    }
-
-    portYIELD_FROM_ISR(task_woken);
 }
--- make/common.mk
+++ make/common.mk
@@ -1,12 +1,9 @@
 PROJECT_DIR := $(shell pwd)
-APP_DIR := $(PROJECT_DIR)/app
-MAIN_DIR := $(APP_DIR)/main
-COMPONENTS_DIR := $(APP_DIR)/components
-THIRD_PARTY_DIR := $(APP_DIR)/third_party
-CORE_DIR := $(PROJECT_DIR)/Core
-STM32CUBEMX_DIR := $(PROJECT_DIR)/cmake/stm32cubemx
+MAIN_DIR := $(PROJECT_DIR)/main
+COMPONENTS_DIR := $(PROJECT_DIR)/components
+THIRD_PARTY_DIR := $(PROJECT_DIR)/third_party
+STM32_CUBEMX_DIR := $(PROJECT_DIR)/stm32_cubemx
 BUILD_DIR := $(PROJECT_DIR)/build
-DRIVERS_DIR := $(PROJECT_DIR)/Drivers
 SCRIPTS_DIR := $(PROJECT_DIR)/scripts
 
 PROJECT_NAME := project
